3bb917065567647ef25eda5952018149f797f7bc
smpnpangalengansatu-panji <smpnpangalengansatu@gmail.com>
2026-04-25 22:14:17 +0700

    Update all3.py

--- all3.py
+++ all3.py
@@ -75,7 +75,7 @@ def apply_custom_ui():
         <div class="main-header">
             <h1>🎓 AA Guru Panji </h1>
             <p>Asisten Administrasi Guru Pangalengan hiji</p>
-			 <p>Created by : Iman Nuriman 2026</p>
+			
         </div>
     """, unsafe_allow_html=True)
 
